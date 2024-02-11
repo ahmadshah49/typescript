9598a332ac6b6cf9d91e6038e509823304ad7789
ahmadshah49 <ahmadraza.fsd.pk94@gmail.com>
2024-02-11 22:42:34 +0500

    commit

--- typescript/app/page.tsx
+++ typescript/app/page.tsx
@@ -1,50 +1,12 @@
-"use client";
-import React, { FormEvent, useState } from "react";
-// import Box from "./components/Box";
+import React from "react";
 
-export default function Home() {
-  type stateType = {
-    name: string;
-    age: number;
-  };
-  const intialState: stateType = {
-    name: "",
-    age: 0,
-  };
-  const [user, setUser] = useState<stateType>(intialState);
-  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setUser(intialState);
-    console.log(user);
-  };
-
+const page = () => {
   return (
-    <>
-      {/* <Box
-        label="input"
-        placeholder="Enter Input"
-        value={val}
-        setter={setVal}
-      /> */}
-      <form onSubmit={submitHandler}>
-        <input
-          placeholder="Enter Age"
-          type="number"
-          value={user?.age}
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
-          }
-        />
-        <input
-          placeholder="Enter Name"
-          type="text"
-          value={user?.name}
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev!, name: e.target.value }))
-          }
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+    <div className="flex justify-center my-8">
+      <h1 className="text-4xl font-bold"> Todo App</h1>
+      
+    </div>
   );
-}
+};
+
+export default page;
